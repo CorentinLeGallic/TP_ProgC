1ca749bfe9231a3ee73cb70db5c5af435f7ea578
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 11:17:47 +0200

    Replaced i++ by j++ boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,7 +4,7 @@ int main() {
   int compteur = 9;
 
   for(int i = 0; i < compteur; i++) {
-    for(int j = 0; j < i; i++){
+    for(int j = 0; j < i; j++){
       printf(i == compteur - 1 || j == 0 || j == i - 1 ? "*" : "#");
     }
     printf("\n");
@@ -12,3 +12,4 @@ int main() {
   
   return 0;
 }
+
