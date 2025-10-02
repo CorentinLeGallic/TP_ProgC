0f9b78b38d4265030dd17fbb4d09c6bc4098c2c4
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 10:25:01 +0200

    Fixed squared rayon in cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,11 +4,12 @@
 int main() {
   float rayon = 10;
 
-  float aire = M_PI * (rayon ** 2);
+  float aire = M_PI * rayon * rayon;
   float perimeter = 2 * M_PI * rayon;
 
   printf("Aire : %f - Périmètre : %f\n", aire, perimeter);
   
   return 0;
 }
+
 
