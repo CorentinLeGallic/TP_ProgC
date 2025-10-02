c8d48b2c21b391049849e3590c9c44c44a898f68
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 11:34:00 +0200

    Added while loop boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+// With for loops
+/*int main() {
   int compteur = 9;
 
   for(int i = 0; i < compteur; i++) {
@@ -11,5 +12,24 @@ int main() {
   }
   
   return 0;
-}
+}*/
 
+// With while loops
+int main() {
+  int compteur = 9;
+
+  int i = 0;
+
+  while(i < compteur){
+    int j = 0;
+
+    while(j < i){
+      printf(i == compteur - 1 || j == 0 || j == i - 1 ? "*" : "#");
+    }
+
+    printf("\n");
+    i++;
+  }
+  
+  return 0;
+}
