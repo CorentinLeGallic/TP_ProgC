f95ea1f1aa66428309c9f0ae5d2adb22b820e31f
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 11:40:44 +0200

    Completed conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int somme = 0;
+  
+  for(int i = 1; i <= 1000; i++) {
+    if(i % 5 == 0 || i % 7 == 0) somme += i;
+    if(i % 11 == 0) continue;
+
+    if(somme > 5000) break;
+  }
+
+  printf("Valeur de la somme : %d", somme);
+
+  return 0;
+}
