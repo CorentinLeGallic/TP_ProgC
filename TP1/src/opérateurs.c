adff02a996ff97b75451520a321dbf3522a3dfb3
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 11:07:10 +0200

    Fixed again % in print in opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -7,10 +7,11 @@ int main() {
   printf("16 + 3 = %d\n", a + b);
   printf("3 - 16 = %d\n", b - a);
   printf("16 * 3 = %d\n", a * b);
-  printf("16 \% 3 = %d\n", a % b);
+  printf("16 modulo 3 = %d\n", a % b);
   printf("16 == 3 = %d\n", a == b);
   printf("16 > 3 = %d\n", a > b);
 
   return 0;
 }
+
 
