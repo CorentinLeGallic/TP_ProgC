97f2416cc6302a8740a1d08487c5c841e99e842a
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 14:19:45 +0200

    First imp in pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(){
+  int n = 9;
+  int pyramidWidth = n*2 - 1;
+
+  for(int i = 1; i <= n; i++){
+    int levelWidth = i*2 - 1;
+    int spaceCounts = (pyramidWidth - levelWidth) / 2;
+
+    for(int j = 0; j < pyramidWidth / 2; j++){
+      if(j < spaceCounts) {
+        printf(" ");
+      } else {
+        printf("%i", j - spaceCounts + 1);
+      }
+    }
+
+    printf("%i", i);
+
+    for(int j = pyramidWidth / 2; j > 0; j--){
+      if(j < spaceCounts) {
+        printf(" ");
+      } else {
+        printf("%i", j - spaceCounts + 1);
+      }
+    }
+  }
+  
+  return 0;
+}
