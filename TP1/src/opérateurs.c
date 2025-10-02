e44047b07b5bedf5d3371696a76dad4ef7a17d67
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 11:05:41 +0200

    Completed opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int a = 16;
+  int b = 3;
+
+  printf("16 + 3 = %d\n", a + b);
+  printf("3 - 16 = %d\n", b - a);
+  printf("16 * 3 = %d\n", a * b);
+  printf("16 % 3 = %d\n", a % b);
+  printf("16 == 3 = %d\n", a == b);
+  printf("16 > 3 = %d\n", a > b);
+
+  return 0;
+}
