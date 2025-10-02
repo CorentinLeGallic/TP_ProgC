3414c1e9642983fac311e1081ef14b09c71a3242
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-02 10:34:55 +0200

    Added linebreaks to sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
 int main() {
-  printf("Size of signed char : %lu", sizeof(signed char));
-  printf("Size of unsigned char : %lu", sizeof(unsigned char));
+  printf("Size of signed char : %lu\n", sizeof(signed char));
+  printf("Size of unsigned char : %lu\n\n", sizeof(unsigned char));
   
-  printf("Size of signed short : %lu", sizeof(signed short));
-  printf("Size of unsigned short : %lu", sizeof(unsigned short));
+  printf("Size of signed short : %lu\n", sizeof(signed short));
+  printf("Size of unsigned short : %lu\n\n", sizeof(unsigned short));
   
-  printf("Size of signed int : %lu", sizeof(signed int));
-  printf("Size of unsigned int : %lu", sizeof(unsigned int));
+  printf("Size of signed int : %lu\n", sizeof(signed int));
+  printf("Size of unsigned int : %lu\n\n", sizeof(unsigned int));
   
-  printf("Size of signed long int : %lu", sizeof(signed long int));
-  printf("Size of unsigned long int : %lu", sizeof(unsigned long int));
+  printf("Size of signed long int : %lu\n", sizeof(signed long int));
+  printf("Size of unsigned long int : %lu\n\n", sizeof(unsigned long int));
   
-  printf("Size of signed long long int : %lu", sizeof(signed long long int));
-  printf("Size of unsigned long long int : %lu", sizeof(unsigned long long int));
+  printf("Size of signed long long int : %lu\n", sizeof(signed long long int));
+  printf("Size of unsigned long long int : %lu\n\n", sizeof(unsigned long long int));
   
-  printf("Size of float : %lu", sizeof(float));
-  printf("Size of double : %lu", sizeof(double));
-  printf("Size of long double : %lu", sizeof(long double));
+  printf("Size of float : %lu\n\n", sizeof(float));
+  
+  printf("Size of double : %lu\n\n", sizeof(double));
+  
+  printf("Size of long double : %lu\n\n", sizeof(long double));
   
   return 0;
 }
+
