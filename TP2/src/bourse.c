87160b0030ea78547440cd2cab882c1edef6a0d4
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-03 16:01:49 +0200

    Completed bourse.c

---
+++ TP2/src/bourse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define DAYS_COUNT 15
+
+int main(){
+  int goldValues[DAYS_COUNT] = { 1503, 1695, 1742, 1566, 2129, 2230, 1591, 1668, 2282, 1689, 2174, 2173, 2290, 1883, 1974 };
+
+  int maximumLoss = 0;
+
+  for(int i = 0; i < DAYS_COUNT; i++){
+    for(int j = 0; i < DAYS_COUNT - i; i++){
+      if(goldValues[j] - goldValues[i] > maximumLoss) maximumLoss = goldValues[j] - goldValues[i];
+    }
+  }
+
+  printf("Maximum loss : %d\n", maximumLoss);
+  
+  return 0;
+}
