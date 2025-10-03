745a9e3ed5711ceafc5fdf1aae1b2ca07e02d13e
CorentinLeGallic <167911929+CorentinLeGallic@users.noreply.github.com>
2025-10-03 14:21:29 +0200

    Adde copyString and concatStrings to chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -11,9 +11,43 @@ int getStringLength(char* string){
   return result;
 }
 
+void copyString(char* string1, char* string2){
+  while(*string1 != '\0'){
+    *string2 = *string1;
+    string1++;
+  }
+}
+
+
+*char concatStrings(char* string1, char* string2){
+  char result[] = string1;
+
+  int i = 0;
+  
+  while(*string2 != '\0'){
+    string1[getStringLength(string1) + i] = *string2;
+    i++;
+    string2++;
+  }
+
+  result[getStringLength(string1) + getStringLength(string2)] = '\0';
+
+  return result;
+}
+
 int main(){
 
-  printf("String length : %d", getStringLength("Coucou"));
-  
+  printf("String length : %d\n", getStringLength("Coucou"));
+
+  char test1[100] = "Hello, ";
+  char test2[100] = "World !";
+
+  printf("Concat string : %s\n", concatStrings(test1, test2));
+
+  copyString(test1, test2);
+
+  printf("Copied string : %s\n", test2);
+
   return 0;
 }
+
